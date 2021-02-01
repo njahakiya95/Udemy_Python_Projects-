93e1cc214c6c5b781f8dd1ee2e2b1425245a496d
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-01 15:46:16 -0500

    finished image recognition project using mobilenet_v2

---
+++ machine_learning/brain.py
@@ -0,0 +1,13 @@
+from imageai.Prediction import ImagePrediction
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "house.jpg"), result_count = 5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+	print(eachPrediction , " : ", eachProbability)
